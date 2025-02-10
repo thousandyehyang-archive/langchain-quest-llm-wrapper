753273dc39e13690afcbb57924773279a5fc6d1a
1000hyehyang <ducogus12@gmail.com>
2025-02-10 14:36:42 +0900

    fix : 서버 연동 및 dotenv

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // https://expressjs.com/
 // https://expressjs.com/en/resources/middleware/cors.html
 
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 
@@ -10,11 +12,19 @@ const port = 3000;
 app.use(cors()); // 미들웨어
 // 모두에게 오픈.
 
+app.use(express.json()); // JSON으로 들어오는 body를 인식
+
 app.get("/", (req, res) => {
   //   res.send("Hello World!");
   res.send("버튼을 눌렀네용!");
   // 수정 후 자동으로 리빌드 되는 건 자연스러운게 아님
   // 무언가 툴들이 도는 거에요... 여기선 nodemon
+});
+
+app.post("/", (req, res) => {
+  const { body } = req;
+  const { text } = body;
+  res.json({ text });
 });
 
 app.listen(port, () => {
