eea06ff9dc4efe933278d5fc994e3c847d686e3c
1000hyehyang <ducogus12@gmail.com>
2025-02-10 13:51:37 +0900

    fix : send 메시지 내용 변경

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(cors()); // 미들웨어
 
 app.get("/", (req, res) => {
   //   res.send("Hello World!");
-  res.send("Bye Earth!");
+  res.send("버튼을 눌렀네용!");
   // 수정 후 자동으로 리빌드 되는 건 자연스러운게 아님
   // 무언가 툴들이 도는 거에요... 여기선 nodemon
 });
 
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`);
+  console.log(`Server running on {port}`);
 });
