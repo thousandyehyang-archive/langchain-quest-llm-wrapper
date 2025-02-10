e03c3aa33473e1d47ce0f59ec25a19731c8487cd
1000hyehyang <ducogus12@gmail.com>
2025-02-10 12:51:17 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+// https://expressjs.com/
+// https://expressjs.com/en/resources/middleware/cors.html
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(cors()); // 미들웨어
+// 모두에게 오픈.
+
+app.get("/", (req, res) => {
+  //   res.send("Hello World!");
+  res.send("Bye Earth!");
+  // 수정 후 자동으로 리빌드 되는 건 자연스러운게 아님
+  // 무언가 툴들이 도는 거에요... 여기선 nodemon
+});
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
